2a3826ff3e1c786884741941009a1d08a1d09e5f
Thiago Mota <th.araujomota@gmail.com>
2022-08-25 11:18:58 -0300

    feat: adds getProductsById tests

--- tests/unit/models/productsModel.test.js
+++ tests/unit/models/productsModel.test.js
@@ -3,7 +3,6 @@ const { describe } = require("mocha");
 const productsModel = require('../../../models/productsModel');
 const connection = require('../../../models/connection');
 const sinon = require('sinon');
-const { object } = require("joi");
 
 describe('getAllProducts', () => {
   afterEach(() => sinon.restore());
@@ -36,8 +35,38 @@ describe('getAllProducts', () => {
   });
 });
 
-// describe('', () => {
-//   it('', () => {
+describe('getProductsByID', () => {
+  afterEach(() => sinon.restore());
 
-//   });
-// });
+  const getProductByIdMock = [[
+    { id: 1, name: 'Martelo de Thor' },
+  ]];
+
+  it('Retorna um array', async () => {
+    sinon.stub(connection, 'execute').resolves(getProductByIdMock);
+    const result = await productsModel.getProductById();
+
+    expect(result).to.be.a('array')
+  });
+
+  it('O array contém um único item', async () => {
+    sinon.stub(connection, 'execute').resolves(getProductByIdMock);
+    const result = await productsModel.getProductById();
+
+    expect(result).to.have.lengthOf(1);
+  });
+
+  it('O array contém itens do tipo objeto', async () => {
+    sinon.stub(connection, 'execute').resolves(getProductByIdMock);
+    const result = await productsModel.getProductById();
+
+    expect(result[0]).to.be.an('object');
+  });
+
+  it('O array retornado contém informações sobre os produtos', async () => {
+    const result = await productsModel.getProductById(1);
+
+    expect(result[0]).to.haveOwnProperty('id');
+    expect(result[0]).to.haveOwnProperty('name');
+  });
+});
