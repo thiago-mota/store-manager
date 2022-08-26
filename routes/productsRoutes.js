17e4a4394507864e87c4508a63e6ae0f228d421f
Thiago Mota <th.araujomota@gmail.com>
2022-08-26 18:10:06 -0300

    feat: applies validations to post/product route

--- routes/productsRoutes.js
+++ routes/productsRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const productsController = require('../controllers/productsController');
+const { validateName, validateNameLength } = require('../middlewares/productsValidation');
 
 const productsRoute = express.Router();
 
 productsRoute.get('/', productsController.getAllProducts);
 productsRoute.get('/:id', productsController.getProductById);
-productsRoute.post('/', productsController.registerNewProduct);
+productsRoute.post('/', validateName, validateNameLength, productsController.registerNewProduct);
 
 module.exports = productsRoute;
