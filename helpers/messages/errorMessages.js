50243829c652de6a890e3a2cbb588eee60be50da
Thiago Mota <th.araujomota@gmail.com>
2022-08-26 18:09:12 -0300

    feat: added new error and status messages

--- helpers/messages/errorMessages.js
+++ helpers/messages/errorMessages.js
@@ -1,6 +1,8 @@
 const errorMessages = {
   PRODUCT_NOT_FOUND: 'Product not found',
   INTERNAL_SERVER_ERROR: 'Internal Server Error',
+  NAME_REQUIRED: '"name" is required',
+  INVALID_NAME: '"name" length must be at least 5 characters long',
 };
 
 module.exports = {
--- helpers/messages/statusMessages.js
+++ helpers/messages/statusMessages.js
@@ -3,6 +3,8 @@ const statusMessages = {
   NOT_FOUND: 404,
   CREATED: 201,
   SERVER_ERROR: 500,
+  BAD_REQUEST: 400,
+  UNPROCESSABLE_ENTITY: 422,
 };
 
 module.exports = {
