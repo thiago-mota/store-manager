21d79a217f175f695ce13d94c9d9f1a6bfdd9f53
Thiago Mota <th.araujomota@gmail.com>
2022-08-27 15:25:15 -0300

    refac: creates getAllSales function on correct file

--- controllers/salesController.js
+++
@@ -1,8 +0,0 @@
-const connection = require('./connection');
-
-const getAllSales = async () => {
-  const [result] = await connection
-    .execute('SELECT * FROM StoreManager.sales;');
-
-  return result;
-};
