044e08916ceb65f9a6439e9833f8dc1fa59aa9eb
Thiago Mota <th.araujomota@gmail.com>
2022-08-27 18:47:22 -0300

    refac: removes try catch

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -3,21 +3,14 @@ const { statusMessages } = require('../helpers/messages/statusMessages');
 const { errorMessages } = require('../helpers/messages/errorMessages');
 
 const getAllProducts = async (_request, response) => {
-  try {
     const result = await productsService.getAllProducts();
 
     return response
       .status(statusMessages.OK)
       .json(result);
-  } catch (error) {
-    return response
-      .status(statusMessages.SERVER_ERROR)
-      .json({ message: errorMessages.INTERNAL_SERVER_ERROR });
-  }
 };
 
 const getProductById = async (request, response) => {
-  try {
     const { id } = request.params;
     const result = await productsService.getProductById(id);
 
@@ -30,26 +23,15 @@ const getProductById = async (request, response) => {
     return response
       .status(statusMessages.OK)
       .json(result);
-  } catch (error) {
-    return response
-      .status(statusMessages.SERVER_ERROR)
-      .json({ message: errorMessages.INTERNAL_SERVER_ERROR });
-  }
 };
 
 const registerNewProduct = async (request, response) => {
-  try {
     const { name } = request.body;
     const result = await productsService.registerNewProduct(name);
 
     return response
       .status(statusMessages.CREATED)
       .json(result);
-  } catch (error) {
-    return response
-      .status(statusMessages.SERVER_ERROR)
-      .json({ message: errorMessages.INTERNAL_SERVER_ERROR });
-  }
 };
 
 module.exports = { getAllProducts, getProductById, registerNewProduct };
