3d3750a9034cc1341eea23ccc339b194003616fb
Thiago Mota <th.araujomota@gmail.com>
2022-08-27 23:59:20 -0300

    feat: adds deleteProduct function to service layer

--- services/productsService.js
+++ services/productsService.js
@@ -3,5 +3,6 @@ const productsModel = require('../models/productsModel');
 const getAllProducts = async () => productsModel.getAllProducts();
 const getProductById = async (id) => productsModel.getProductById(id);
 const registerNewProduct = async (name) => productsModel.registerNewProduct(name);
+const deleteProduct = async (id) => productsModel.deleteProduct(id);
 
-module.exports = { getAllProducts, getProductById, registerNewProduct };
+module.exports = { getAllProducts, getProductById, registerNewProduct, deleteProduct };
