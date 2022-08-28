6dbb716111110db9481100f5171db6c12d078b09
Thiago Mota <th.araujomota@gmail.com>
2022-08-28 13:16:20 -0300

    feat: adds deleteSale function to service layer

--- services/salesService.js
+++ services/salesService.js
@@ -2,5 +2,6 @@ const salesModel = require('../models/salesModel');
 
 const getAllSales = async () => salesModel.getAllSales();
 const getSaleById = async (id) => salesModel.getSaleById(id);
+const deleteSale = async (id) => salesModel.deleteSale(id);
 
 module.exports = { getAllSales, getSaleById };
