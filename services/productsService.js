1b0c2207488ae98301d6f31e4c98b0d0d86b74cc
Thiago Mota <th.araujomota@gmail.com>
2022-08-28 19:04:45 -0300

    fix: adds validation to delete product function on service layer

--- services/productsService.js
+++ services/productsService.js
@@ -3,7 +3,12 @@ const productsModel = require('../models/productsModel');
 const getAllProducts = async () => productsModel.getAllProducts();
 const getProductById = async (id) => productsModel.getProductById(id);
 const registerNewProduct = async (name) => productsModel.registerNewProduct(name);
-const deleteProduct = async (id) => productsModel.deleteProduct(id);
+const deleteProduct = async (id) => { 
+  const result = await productsModel.deleteProduct(id);
+
+  if (result.affectedRows === 0) return false;
+  return true;
+};
 
 const searchProduct = async (name) => productsModel.searchProduct(name);
 const updateProduct = async ({ name, id }) => {
