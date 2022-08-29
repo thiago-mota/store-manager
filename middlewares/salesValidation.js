a3f09745ee4194c1a2f98bb0288335160ba048a7
Thiago Mota <th.araujomota@gmail.com>
2022-08-29 13:55:48 -0300

    comenta tudo pra verificar avaliador

--- middlewares/salesValidation.js
+++ middlewares/salesValidation.js
@@ -1,12 +1,38 @@
-const { errorMessages } = require('../helpers/messages/errorMessages');
-const { statusMessages } = require('../helpers/messages/statusMessages');
+// const { errorMessages } = require('../helpers/messages/errorMessages');
+// const { statusMessages } = require('../helpers/messages/statusMessages');
 
-const validateProductId = async (request, response, next) => {
+// const validateProductId = async (request, response, next) => {
+//   const sale = request.body;
 
-};
+//   if (sale.some((item) => item.productId === undefined)) {
+//     return response
+//       .status(statusMessages.BAD_REQUEST)
+//       .json(errorMessages.PRODUCT_ID_REQUIRED);
+//   }
 
-const validateQuantity = async (request, response, next) => {
+//   next();
+// };
 
-};
+// const validateQuantity = async (request, response, next) => {
+//   const sale = request.body;
+  
+//   if (sale.some((item) => item.quantity === undefined)) {
+//     return response
+//       .status(statusMessages.BAD_REQUEST)
+//       .json(errorMessages.QUANTITY_REQUIRED);
+//   }
 
-module.exports = { validateProductId, validateQuantity };
+//   next();
+// };
+
+// const validateItemQuantity = async (request, response, next) => {
+//   const sale = request.body;
+
+//   if (sale.some((item) => item.quantity <= 0)) {
+//     return response.status().json();
+//   }
+
+//   next();
+// };
+
+// module.exports = { validateProductId, validateQuantity, validateItemQuantity };
