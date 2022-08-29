89635d95f03e2ed0d17e0221db556123a726358d
Thiago Mota <th.araujomota@gmail.com>
2022-08-29 00:23:38 -0300

    feat: creates salesValidation file

---
+++ middlewares/salesValidation.js
@@ -0,0 +1,12 @@
+const { errorMessages } = require('../helpers/messages/errorMessages');
+const { statusMessages } = require('../helpers/messages/statusMessages');
+
+const validateProductId = async (request, response, next) => {
+
+};
+
+const validateQuantity = async (request, response, next) => {
+
+};
+
+module.exports = { validateProductId, validateQuantity };
